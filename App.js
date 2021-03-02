488c8ea7ff737210db38c4689a46f51a0ed0962b
Cristina Rodriguez <cristina@Cristinas-MacBook-Pro.local>
2021-03-01 18:53:24 -0800

    Basic list working

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredInput, setInput] = useState('');
+  const [gratitudeList, setGratitudeItem] = useState([]);
+
+  const inputHandler = (enteredText) => {
+    setInput(enteredText);
+  };
+
+  const addInputHandler = () => {
+    //console.log(enteredInput);
+    setGratitudeItem(currentItem => [...gratitudeList, enteredInput]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenLayout} >
+     <View style={styles.inputContainer}>
+       <TextInput 
+       placeholder="Express gratitude for" 
+       style={styles.textContainer} 
+       onChangeText={inputHandler}
+       value={enteredInput}/>
+      <Button title="Add" onPress={addInputHandler}></Button>
+    </View>
+
+     <View>
+      {gratitudeList.map((item, index) => <Text key={item}>{item}</Text>)}
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screenLayout: {
+    padding: 50
   },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: 'center'
+  },
+  textContainer:{
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 5
+  }
 });
